680446a743e340eb590cec49ca1eb4d06c3f363a
AVCTY <arthurchin8817@gmail.com>
2024-01-17 23:43:28 +0800

    added main.py and some code

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import wavelink
+import discord
+from discord.ext import commands
+
+with open("secrets.json", "r") as f:
+    data = json.load(f)
+
+BOT_TOKEN = data["BOT_TOKEN"]
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="d!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.event
+async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
+    print(f"Node {payload.node!r} is ready!")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}, I am Donut Music Bot!")
+
+@bot.command()
+async def stop(ctx):
+    pass
+
+bot.run(BOT_TOKEN)
