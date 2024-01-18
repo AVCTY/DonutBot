b5fa0038b10e63f3b2614d5869297a3fab4105da
AVCTY <arthurchin8817@gmail.com>
2024-01-18 18:41:11 +0800

    added basic music features in main, and added application settings for lavalink node connection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import json
+import asyncio
+import logging
+from typing import cast
+
 import wavelink
+
 import discord
 from discord.ext import commands
 
@@ -8,25 +13,106 @@ with open("./Assets/secrets.json", "r") as f:
 
 BOT_TOKEN = data["BOT_TOKEN"]
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix="d!", intents=intents)
+# Bot class
+class Bot(commands.Bot):
+    def __init__(self) -> None:
+        intents: discord.Intents = discord.Intents.default()
+        intents.message_content = True
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name} ({bot.user.id})")
-    await bot.change_presence(activity=discord.Game(name="Listening to d!play"))
+        discord.utils.setup_logging(level=logging.INFO)
+        super().__init__(command_prefix="d!", intents=intents)
 
-@bot.event
-async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
-    print(f"Node {payload.node!r} is ready!")
+    async def setup_hook(self) -> None:
+        nodes = [wavelink.Node(uri="http://localhost:2333", password="youshallnotpass")]
 
+        await wavelink.Pool.connect(nodes=nodes, client=self)
+
+    async def on_ready(self) -> None:
+        logging.info(f"Logged in: {self.user} | {self.user.id}")
+
+    async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
+        logging.info(f"Wavelink Node connected: {payload.node!r} | Resumed: {payload.resumed}")
+
+
+bot = Bot()
+
+# Play command
 @bot.command()
-async def test(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}, I am Donut Music Bot!")
+async def play(ctx: commands.Context, query: str) -> None:
+    if not ctx.guild:
+        return
+    
+    player: wavelink.Player
+    player = cast(wavelink.player, ctx.voice_client)
 
+    if not player:
+        try:
+            player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        except AttributeError:
+            await ctx.send("Please join a voice channel first before using this command.")
+            return
+        except discord.ClientException:
+            await ctx.send("I was unable to join this voice channel. Please try again.")
+            return
+        
+    # Turn on AutoPlay
+    # enabled = AutoPlay will automatically play songs in the queue and fetch recommendations after queue finishes
+    # partial = AutoPlay will automatically play songs in the queue but will not fetch recommendations after queue finishes
+    # disabled = AutoPlay will do nothing
+    player.autoplay = wavelink.AutoPlayMode.partial # set to only play songs in queue without fetching recommendations after
+
+    # Lock player to this channel
+    if not hasattr(player, "home"):
+        player.home = ctx.channel
+    elif player.home != ctx.channel:
+        await ctx.send(f"You can only play songs in {player.home.mention}, as the player has already started there.")
+        return
+    
+
+    # This will handle fetching Tracks and Playlists...
+    # Seed the doc strings for more information on this method...
+    # If spotify is enabled via LavaSrc, this will automatically fetch Spotify tracks if you pass a URL...
+    # Defaults to YouTube for non URL based queries...
+    tracks: wavelink.Search = await wavelink.Playable.search(query)
+    if not tracks:
+        await ctx.send(f"{ctx.author.mention} - Could not find any tracks with that query. Please try again.")
+        return
+
+    if isinstance(tracks, wavelink.Playlist):
+        # tracks is a playlist...
+        added: int = await player.queue.put_wait(tracks)
+        await ctx.send(f"Added the playlist **`{tracks.name}`** ({added} songs) to the queue.")
+    else:
+        track: wavelink.Playable = tracks[0]
+        await player.queue.put_wait(track)
+        await ctx.send(f"Added **`{track}`** to the queue.")
+
+    if not player.playing:
+        # Play now since we aren't playing anything...
+        await player.play(player.queue.get(), volume=30)
+
+
+# Skip command
 @bot.command()
-async def stop(ctx):
-    pass
+async def skip(ctx: commands.Context) -> None:
+    """Skip the current song."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+
+    await player.skip(force=True)
+    await ctx.message.add_reaction("\u2705")
+
+
+# Disconnect command
+@bot.command(aliases=["dc"])
+async def disconnect(ctx: commands.Context) -> None:
+    """Disconnect the Player."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+
+    await player.disconnect()
+    await ctx.message.add_reaction("\u2705")
 
 bot.run(BOT_TOKEN)
